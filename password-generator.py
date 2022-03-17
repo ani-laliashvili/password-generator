d749e673f230cce055a27c36bae994d00ff5afea
ani-laliashvili <71911564+ani-laliashvili@users.noreply.github.com>
2022-03-17 17:08:30 -0400

    Update password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -5,15 +5,12 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the Password Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_char = int(input("How many characters would you like in your password?\n"))
 
 char_array = [letters, numbers, symbols]
 password = ''
 
-for x in range(0, nr_letters + nr_numbers + nr_symbols):
-    chosen_array = char_array[random.randint(0, len(char_array) - 1)]
-    password += chosen_array[random.randint(0, len(chosen_array) - 1)]
+for x in range(0, nr_char):
+    password += random.choice(random.choice(char_array))
 
 print(f"Your password is: {password}")
